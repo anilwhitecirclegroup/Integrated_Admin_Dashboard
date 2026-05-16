1f259c028d880d8395aae2e53cdb39de8a6213b4
AnilYadav751 <anilyad292@gmail.com>
2026-05-16 18:35:18 +0530

    Updated the conference-room.html file with restricted date selectors

--- admindashboard/admindashboard/src/main/java/com/example/admindashboard/security/WebSocketConfig.java
+++ admindashboard/admindashboard/src/main/java/com/example/admindashboard/security/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.admindashboard.config;
+package com.example.admindashboard.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
