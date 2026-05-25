f317fdef738d43a6ca387a885f7d45acece712d5
Aaditya Prabhakar <aadityawhitecirclegroup@yahoo.com>
2026-05-25 18:48:54 +0530

    Implement leave wallet and leave ledger module

---
+++ admindashboard/admindashboard/src/main/java/com/example/admindashboard/model/LeaveLedger.java
@@ -0,0 +1,5 @@
+package com.example.admindashboard.model;
+
+public class LeaveLedger {
+    
+}
---
+++ admindashboard/admindashboard/src/main/java/com/example/admindashboard/repository/LeaveTypeMasterRepository.java
@@ -0,0 +1,5 @@
+package com.example.admindashboard.repository;
+
+public class LeaveTypeMasterRepository {
+    
+}
